5448877d87839b5f6c1bf44c7076c18da7a4e5cf
BKR-dev <giga@sigmachad.quest>
2023-10-24 17:55:57 +0200

    it works

--- main.go
+++ main.go
@@ -8,28 +8,20 @@ import (
 )
 
 func main() {
-	component := hello("John", getCurrentTime().String())
-	timeComponent := returnTime(getCurrentTime().String())
+	component := hello("Valentine")
+	timeComponent := returnTime()
 
 	r := chi.NewRouter()
 	// serves the hello component at root
 	r.Get("/", templ.Handler(component).ServeHTTP)
 	// serves only the timestamp div
 	r.Get("/time", templ.Handler(timeComponent).ServeHTTP)
-	// serves only the timestamp div
-	r.Get("/time2", timeHandler)
 
 	// starting the server
 	println("Listening on :3000")
 	http.ListenAndServe(":3000", r)
 }
 
-func getCurrentTime() time.Time {
+func GetCurrentTime() time.Time {
 	return time.Now().UTC()
-}
-
-func timeHandler(w http.ResponseWriter, r *http.Request) {
-	message := `<div id="timestamp">at current time: `
-	message = message + getCurrentTime().String() + `</div>`
-	w.Write([]byte(message))
 }
