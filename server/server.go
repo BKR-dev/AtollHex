8a1b0629d080662209ad69d18cca96ae0308271e
BKR-dev <giga@sigmachad.quest>
2023-11-19 15:41:02 +0100

    i think im starting to learn templ

--- server/server.go
+++ server/server.go
@@ -1,49 +1,66 @@
 package server
 
 import (
-	"encoding/json"
 	"net/http"
-	"strconv"
+	"time"
 
 	"github.com/BKR-dev/AtollHex/logic"
 	"github.com/BKR-dev/AtollHex/view"
-
-	"github.com/a-h/templ"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
+
+// func NewServer() *http.Server {
+// 	currentTime := logic.GetCurrentTime().String()
+// 	component := view.PageView("AtollHEX")
+// 	timeComponent := view.ReturnTime(currentTime)
+
+// 	r := chi.NewRouter()
+
+// 	r.Use(middleware.Logger)
+// 	r.Use(middleware.Recoverer)
+// 	// serves the hello component at root
+// 	r.Get("/", templ.Handler(component).ServeHTTP)
+// 	// serves only the timestamp div
+// 	r.Get("/time", templ.Handler(timeComponent).ServeHTTP)
+// 	// serves articles
+// 	r.Get("/articles", func(w http.ResponseWriter, r *http.Request) {
+// 		page, _ := strconv.Atoi(r.URL.Query().Get("page"))
+
+// 		// Fetch articles based on page and pageSize
+// 		// This is just a placeholder. Replace it with your actual logic.
+// 		articles := view.ReturnArticles(page)
+
+// 		// Convert articles to JSON and write to response
+// 		// This is also a placeholder. Replace it with your actual logic.
+// 		json.NewEncoder(w).Encode(articles)
+// 	})
+
+// 	// create a new http.Server
+// 	server := &http.Server{
+// 		Addr:    ":3000",
+// 		Handler: r,
+// 	}
+
+// 	return server
+// }
+
+// NowHandler struct the returns the current time in a anonymous function
+type NowHandler struct {
+	Now func() time.Time
+}
+
+// NewNowHandler returns a new NowHandler
+func NewNowHandler(now func() time.Time) NowHandler {
+	return NowHandler{now}
+}
+
+// ServeHTTP confirms to the http.Handler interface on the NowHandler struct
+func (nh NowHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	view.TimeComponent(nh.Now().String()).Render(r.Context(), w)
+}
 
 func NewServer() *http.Server {
 	currentTime := logic.GetCurrentTime().String()
-	component := view.PageView("AtollHEX")
+	pageViewComponent := view.PageView("AtollHEX", currentTime)
 	timeComponent := view.ReturnTime(currentTime)
-
-	r := chi.NewRouter()
-
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	// serves the hello component at root
-	r.Get("/", templ.Handler(component).ServeHTTP)
-	// serves only the timestamp div
-	r.Get("/time", templ.Handler(timeComponent).ServeHTTP)
-	// serves articles
-	r.Get("/articles", func(w http.ResponseWriter, r *http.Request) {
-		page, _ := strconv.Atoi(r.URL.Query().Get("page"))
-
-		// Fetch articles based on page and pageSize
-		// This is just a placeholder. Replace it with your actual logic.
-		articles := view.ReturnArticles(page)
-
-		// Convert articles to JSON and write to response
-		// This is also a placeholder. Replace it with your actual logic.
-		json.NewEncoder(w).Encode(articles)
-	})
-
-	// create a new http.Server
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: r,
-	}
-
-	return server
+	http.Handle("/", NewNowHandler(time.Now))
 }
